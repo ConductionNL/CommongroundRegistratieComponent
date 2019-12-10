6a701cf6e3302d5ff34052f1e46e789a1437e179
Ruben van der Linde <rubenvdlinde@gmail.com>
2019-12-10 15:12:26 +0000

    Apply fixes from StyleCI

--- api/src/Controller/Add.php
+++ api/src/Controller/Add.php
@@ -1,30 +1,28 @@
 <?php
+
 // api/src/Controller/AddController.php
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\RequestStack;
-
 use App\Service\AddService;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class Add
 {
     protected $requestStack;
     protected $addService;
-    
+
     public function __construct(RequestStack $requestStack, AddService $addService)
     {
         $this->requestStack = $requestStack;
-        $this->addService   = $addService;
+        $this->addService = $addService;
     }
-    
+
     public function __invoke()
     {
         $request = $this->requestStack->getCurrentRequest();
         $url = $request->query->get('url');
-        
-        
+
         return $this->addService->add($url);
     }
-    
 }
--- api/src/Controller/ComponentRefresh.php
+++ api/src/Controller/ComponentRefresh.php
@@ -1,24 +1,25 @@
 <?php
+
 // api/src/Controller/ComponentController.php
 
 namespace App\Controller;
 
-use App\Service\ComponentService;
 use App\Entity\Component;
+use App\Service\ComponentService;
 
-class ComponentRefresh  
+class ComponentRefresh
 {
     private $componentService;
-    
+
     public function __construct(ComponentService $componentService)
     {
         $this->componentService = $componentService;
     }
-    
+
     public function __invoke(Component $data): Component
     {
         $data = $this->componentService->refresh($data);
-        
+
         return $data;
     }
 }
